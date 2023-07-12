db326bdd7324d751cf82ea9982eebfeb315c66a3
Victor Hernandez <victormanu968@gmail.com>
2023-07-12 13:23:07 -0500

    mejorando interfaz, se limpian campos

--- src/vistas/VistasComponentes/VistaActualizar.java
+++ src/vistas/VistasComponentes/VistaActualizar.java
@@ -15,6 +15,13 @@ public class VistaActualizar extends javax.swing.JFrame {
         initComponents();
     }
 
+    public void limpiarCampos(){
+        txtNombre.setText("");
+        txtPais.setText("");
+        txtMaterial.setText("");
+        txtPrecio.setText("");
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
