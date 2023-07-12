5a9786f79036696bc677bda4857e3dd579ded98b
jessivilla1975 <yessica.villa@correounivalle.edu.co>
2023-07-12 09:27:32 -0500

    se corrige error 2

--- src/vistas/VistasComponentes/VistaActualizar.java
+++ src/vistas/VistasComponentes/VistaActualizar.java
@@ -1,14 +1,5 @@
-package vistas.vistasComponentes;
+package vistas.VistasComponentes;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
-
-/**
- *
- * @author Victor
- */
 public class VistaActualizar extends javax.swing.JFrame {
 
     /**
