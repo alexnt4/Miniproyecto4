4a6be7c44f6d10afa62f726d694b13d3919451fd
Alex García Castañeda <kend1123ue@gmail.com>
2023-07-12 09:09:01 -0500

    estructura controlador

--- src/controlador/ControladorTrajes.java
+++ src/controlador/ControladorTrajes.java
@@ -1,5 +1,57 @@
 package controlador;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class ControladorTrajes {
-    
+import modelo.Modelo;
+import vistas.IVista;
+
+
+
+public class ControladorTrajes implements ActionListener {
+    Modelo modelo;
+    IVista vista;
+    Operaciones operacion;
+
+    public ControladorTrajes(Modelo modelo, IVista vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+    }
+
+    public Operaciones getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(Operaciones operacion) {
+        this.operacion = operacion;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        switch (operacion) {
+            case INSERTAR:
+
+                break;
+            case ACTUALIZAR:
+                    
+                    break;
+            case ELIMINAR:
+                        
+                    break;
+            case BUSCAR:
+                            
+                    break;
+            case LISTAR:
+                                
+                    break;
+            case COMPRAR:
+                            
+                    break;
+            default:
+                break;
+        }      
+    }
 }
+
+
+
+
